4b0250465799000db796a8004dfcfc9efc590c5d
vikasatawww <142781509+vikasatawww@users.noreply.github.com>
2023-09-13 08:30:48 -0700

    Added Dashboard.js file

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      {/* Dashboard content here */}
+    </div>
+  );
+}
+
+export default Dashboard;
